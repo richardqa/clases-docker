cbb3104a263a1c8cac60829a6fc316924751d40b
Javier Quinto <richardqa@gmail.com>
2023-07-16 06:39:16 +0000

    update

--- compose-example/basic-python/simple.py
+++ compose-example/basic-python/simple.py
@@ -5,15 +5,15 @@ import socket
 ## Use an ongoing while loop to generate output
 while True :
 ## Set the hostname and the current date
-host = socket.gethostname()
-date = time.strftime("%Y-%m-%d %H:%M:%S")
+  host = socket.gethostname()
+  date = time.strftime("%Y-%m-%d %H:%M:%S")
 ## Convert the date output to a string
-now = str(date)
+  now = str(date)
 ## Open the file named date in append mode
 ## Append the output of hostname and time
-f = open("date.out", "a" )
-f.write(now + "\n")
-f.write(host + "\n")
-f.close()
+  f = open("date.out", "a" )
+  f.write(now + "\n")
+  f.write(host + "\n")
+  f.close()
 ## Sleep for five
-time.sleep(5)
+  time.sleep(5)
